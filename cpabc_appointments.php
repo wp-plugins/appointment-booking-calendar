a59ad5c68b315f1c8e6a05b5fbf0d544c4327373
codepeople <codepeople@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-01-27 12:54:15 +0000

    Fixed query used for installation in multisite environments.
    git-svn-id: https://plugins.svn.wordpress.org/appointment-booking-calendar/trunk@1076627 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- cpabc_appointments.php
+++ cpabc_appointments.php
@@ -155,7 +155,7 @@ function cpabc_appointments_install($networkwide)  {
 		if ($networkwide) {
 	                $old_blog = $wpdb->blogid;
 			// Get all blog ids
-			$blogids = $wpdb->get_col($wpdb->prepare("SELECT blog_id FROM $wpdb->blogs"));
+			$blogids = $wpdb->get_col( "SELECT blog_id FROM $wpdb->blogs" );
 			foreach ($blogids as $blog_id) {
 				switch_to_blog($blog_id);
 				_cpabc_appointments_install();
