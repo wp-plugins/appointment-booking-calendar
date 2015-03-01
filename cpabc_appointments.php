1ea46a947ff9034c1c974f78121eafe1dcf165f7
codepeople <codepeople@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-03-01 23:46:52 +0000

    Added code to automatically repair corrupted appointment databases.
    git-svn-id: https://plugins.svn.wordpress.org/appointment-booking-calendar/trunk@1102869 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- cpabc_appointments.php
+++ cpabc_appointments.php
@@ -1346,6 +1346,8 @@ function cpabc_appointments_calendar_load() {
                 $working_dates[$i] = intval($working_dates[$i]);
             else            
                 $working_dates[$i] = ''; 
+        if ($working_dates[0] == '')
+            unset($working_dates[0]);                
         $working_dates = array_unique($working_dates);        
         $working_dates = implode(",",$working_dates); 
         while (!(strpos($working_dates,",,") === false))
